404e24a22188dde8a377bb507316bdcfcb27ec1e
satchelaviram <satchelaviram@gmail.com>
2016-10-26 14:47:54 -0400

    Create node.rb

---
+++ node.rb
@@ -0,0 +1,92 @@
+$port = nil
+$hostname = nil
+
+
+
+# --------------------- Part 0 --------------------- # 
+
+def edgeb(cmd)
+	STDOUT.puts "EDGE: not implemented"
+end
+
+def dumptable(cmd)
+	puts "DUMPTABLE: not implemented"
+end
+
+def shutdown(cmd)
+	STDOUT.puts "SHUTDOWN: not implemented"
+	exit(0)
+end
+
+
+
+# --------------------- Part 1 --------------------- # 
+def edged(cmd)
+	STDOUT.puts "EDGED: not implemented"
+end
+
+def edgew(cmd)
+	STDOUT.puts "EDGEW: not implemented"
+end
+
+def status()
+	STDOUT.puts "STATUS: not implemented"
+end
+
+
+# --------------------- Part 2 --------------------- # 
+def sendmsg(cmd)
+	STDOUT.puts "SENDMSG: not implemented"
+end
+
+def ping(cmd)
+	STDOUT.puts "PING: not implemented"
+end
+
+def traceroute(cmd)
+	STDOUT.puts "TRACEROUTE: not implemented"
+end
+
+def ftp(cmd)
+	STDOUT.puts "FTP: not implemented"
+end
+
+
+
+
+# do main loop here.... 
+def main()
+
+	while(line = STDIN.gets())
+		line = line.strip()
+		arr = line.split(' ')
+		cmd = arr[0]
+		args = arr[1..-1]
+		case cmd
+		when "EDGEB"; edgeb(args)
+		when "EDGED"; edged(args)
+		when "EDGEW"; edgew(args)
+		when "DUMPTABLE"; dumptable(args)
+		when "SHUTDOWN"; shutdown(args)
+		when "STATUS"; status()
+		when "SENDMSG"; sendmsg(args)
+		when "PING"; ping(args)
+		when "TRACEROUTE"; traceroute(args)
+		when "FTP"; ftp(args)
+		else STDERR.puts "ERROR: INVALID COMMAND \"#{cmd}\""
+		end
+	end
+
+end
+
+def setup(hostname, port)
+	$hostname = hostname
+	$port = port
+
+	#set up ports, server, buffers
+
+	main()
+
+end
+
+setup(ARGV[0], ARGV[1])
