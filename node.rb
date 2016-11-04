c323f35be65bb93ac0765d87726e7126637eac2e
Satchel Aviram <satchelaviram@Satchels-MacBook-Air.local>
2016-11-04 16:03:17 -0400

    commit

--- node.rb
+++ node.rb
@@ -1,108 +1,113 @@
 $port = nil
 $hostname = nil
-
+$node_info = nil
+$rt = nil
 require 'socket'
-
-
 
 # --------------------- Part 0 --------------------- # 
 
-def edgeb(edge, srcip, dstip, dst)
-	rt = Hash.new
-	edgeHash = Hash.new
+def edgeb(cmd)
+    
+    #s = Socket.new Socket::INET, Socket::SOCK_STREAM
+    #s.connect Socket.pack_sockaddr_in($port, srcip)
 
-	s = Socket.new Socket::INET, Socket::SOCK_STREAM
-	s.connect Socket.pack_sockaddr_in($port, srcip)
+    #serv = TCPServer.new(dstip, $port)
+    #s = serv.accept
 
-	serv = TCPServer.new(dstip, $port)
-	s = serv.accept
+    if($rt[$hostname] != nil)
+    	$rt[$hostname].nexthop = cmd[2]
+    else
+    	node = $node_info.new
+    	node.src = $hostname
+    	node.dst = cmd[2]
+    	node.cost = 1
+    	node.nexthop = cmd[2]
+    	$rt[$hostname] = node
+    end
 
-
-	edgeHash[edge] = 1
-	rt[dst] = edgeHash
-
-
-		
 
 end
 
 def dumptable(cmd)
-	puts "DUMPTABLE: not implemented"
+	file = File.open(cmd[0], 'w')
+	$rt.each {|node, str| file.write "#{str[:src]},#{str[:dst]},#{str[:nexthop]},#{str[:cost]}\n"}
 end
 
 def shutdown(cmd)
-	STDOUT.puts "SHUTDOWN: not implemented"
-	exit(0)
+    STDOUT.flush
+    close(serv)
+    exit(0)
 end
-
 
 
 # --------------------- Part 1 --------------------- # 
 def edged(cmd)
-	STDOUT.puts "EDGED: not implemented"
+    STDOUT.puts "EDGED: not implemented"
 end
 
 def edgew(cmd)
-	STDOUT.puts "EDGEW: not implemented"
+    STDOUT.puts "EDGEW: not implemented"
 end
 
 def status()
-	STDOUT.puts "STATUS: not implemented"
+    STDOUT.puts "STATUS: not implemented"
 end
 
 
 # --------------------- Part 2 --------------------- # 
 def sendmsg(cmd)
-	STDOUT.puts "SENDMSG: not implemented"
+    STDOUT.puts "SENDMSG: not implemented"
 end
 
 def ping(cmd)
-	STDOUT.puts "PING: not implemented"
+    STDOUT.puts "PING: not implemented"
 end
 
 def traceroute(cmd)
-	STDOUT.puts "TRACEROUTE: not implemented"
+    STDOUT.puts "TRACEROUTE: not implemented"
 end
 
 def ftp(cmd)
-	STDOUT.puts "FTP: not implemented"
+    STDOUT.puts "FTP: not implemented"
 end
-
-
 
 
 # do main loop here.... 
 def main()
 
-	while(line = STDIN.gets())
-		line = line.strip()
-		arr = line.split(' ')
-		cmd = arr[0]
-		args = arr[1..-1]
-		case cmd
-		when "EDGEB"; edgeb(args)
-		when "EDGED"; edged(args)
-		when "EDGEW"; edgew(args)
-		when "DUMPTABLE"; dumptable(args)
-		when "SHUTDOWN"; shutdown(args)
-		when "STATUS"; status()
-		when "SENDMSG"; sendmsg(args)
-		when "PING"; ping(args)
-		when "TRACEROUTE"; traceroute(args)
-		when "FTP"; ftp(args)
-		else STDERR.puts "ERROR: INVALID COMMAND \"#{cmd}\""
-		end
-	end
+    while(line = STDIN.gets())
+        line = line.strip()
+        arr = line.split(' ')
+        cmd = arr[0]
+        args = arr[1..-1]
+        case cmd
+        when "EDGEB"; edgeb(args)
+        when "EDGED"; edged(args)
+        when "EDGEW"; edgew(args)
+        when "DUMPTABLE"; dumptable(args)
+        when "SHUTDOWN"; shutdown(args)
+        when "STATUS"; status()
+        when "SENDMSG"; sendmsg(args)
+        when "PING"; ping(args)
+        when "TRACEROUTE"; traceroute(args)
+        when "FTP"; ftp(args)
+        else STDERR.puts "ERROR: INVALID COMMAND \"#{cmd}\""
+        end
+    end
 
 end
 
-def setup(hostname, port)
-	$hostname = hostname
-	$port = port
+def setup(hostname, port, nodes, config)
+    $hostname = hostname
+    $port = port
+    $rt = Hash.new
+    $node_info = Struct.new(:src, :dst, :cost, :nexthop)
+    #set up ports, server, buffers
 
-	#set up ports, server, buffers
 
-	main()
+    
+
+    main()
 
 end
 
