5bec2f638abc0d2acdf8685e7ebc4da70ec7b3ad
Satchel Aviram <satchelaviram@Satchels-Air.fios-router.home>
2016-11-13 22:19:48 -0500

    commit

--- node.rb
+++ node.rb
@@ -1,75 +1,122 @@
 require 'socket'
 require 'thread'
 
-$p = nil
-$hostname = nil
-$node_info = nil
-$rt = Hash.new 
-$serv = nil
-$nodesFile = {}
-$clock = nil
+$p = nil #Port node is listening on
+$hostname = nil #Nodes name
+$node_info = nil #Struct for nodes rt
+$rt = Hash.new #{destNode,Nodestruct}Basic Routing Table made in part 0. May be redone to graph for dijkstras
+$serv = nil #Server for that handles messages from other nodes
+$nodesFile = {} # A hash mapping {srcNode, {string, /Node info/}} from nodes file
+$clock = nil #Clock to keep the time of the program
+$local_ip = nil #Local Ip given in
+$lock = Mutex.new #Lock to ensure the program is thread safe
+$record_ip = nil
 
 # --------------------- Part 0 --------------------- # 
 def edgeb_stdin(cmd)
+    $record_ip[$hostname] = cmd[0]
+    $record_ip[cmd[2]] = cmd[1]
 
-    node = $node_info.new   
-    node.src = $hostname
-    node.dst = cmd[2]
-    node.cost = 1
-    node.nexthop = cmd[2] 
-    $rt[$hostname] = node
-
-    client = TCPSocket.open(cmd[0], $p)
-    client.puts("#{cmd[2]},#{$hostname}")
-    edgeb_network(cmd[0])
-    client.close
+    lock.synchronize{
+        node = $node_info.new   
+        node.src = $hostname
+        node.dst = cmd[2]
+        node.cost = 1
+        node.nexthop = cmd[2] 
+        $rt[cmd[2]] = node
+        if $local_ip == nil then local_ip = cmd[0] end
+    }
+    client = TCPSocket.open(cmd[1], $nodesFile[cmd[2]]["PORT"])
+    client.puts("EDGEB2,#{cmd[2]},#{$hostname},#{cmd[1]}")     
 
 end
 
 def edgeb_network(cmd)
 
-    thread = Thread.start($serv.accept) do |client|
+    puts "I AM HERE"
 
-        message = client.gets.chomp
-
-        arr = message.split(/,/)
-        my_name = arr[0]
-        src_name = arr[1]
-
-        node1 = $node_info.new        
-        node1.src = my_name
-        node1.dst = src_name
-        node1.cost = 1
-        node1.nexthop = src_name
-        $rt[my_name] = node1
-    end
-
-    thread.join
+    lock.synchronize{
+        node = $node_info.new   
+        node.src = $hostname
+        node.dst = cmd[1]
+        node.cost = 1
+        node.nexthop = cmd[1] 
+        $rt[cmd[1]] = node
+        if $local_ip == nil then local_ip = cmd[2] end
+    }
+    puts "THIS IS THE ROUTING TABLE: #{$rt}"
 
 end
 
 def dumptable(cmd)
+    sleep(1)
     file = File.open(cmd[0], 'w')
+    puts "ABOUT TO PRINT THE ROUTING TABLE: #{$rt}"
     $rt.each {|node, str| file.write "#{str[:src]},#{str[:dst]},#{str[:nexthop]},#{str[:cost]}\n"}
 end
 
 def shutdown(cmd)
     STDOUT.flush
+    Thread.list.each do |thread|
+          thread.exit unless thread == Thread.current
+    end
     exit(0)
 end
 
-
 # --------------------- Part 1 --------------------- # 
 def edged(cmd)
-    STDOUT.puts "EDGED: not implemented"
+    ip = record_ip[$hostname]
+
+    $rt.each {|node, str| 
+        if str[:dst] == cmd[0]
+            str = nil
+        end
+        client = TCPSocket.open(ip, $nodesFile[str[:nexthop]]["PORT"])
+        client.puts("EDGEU2,#{cmd[0]},#{cmd[1]}") 
+    }
 end
 
-def edgew(cmd)
-    STDOUT.puts "EDGEW: not implemented"
+def edged_network(cmd)
+    lock.synchronize{
+        $rt.each {|node, str| 
+            if str[:dst] == cmd[0]
+                str = nil
+            end
+        }
+    }
+end
+
+
+def edgeu(cmd)
+    ip = record_ip[$hostname]
+
+    $rt.each {|node, str| 
+        if str[:dst] == cmd[0]
+            str[:cost] = cmd[1]
+        end
+        client = TCPSocket.open(ip, $nodesFile[str[:nexthop]["PORT"])
+        client.puts("EDGEU2,#{cmd[0]},#{cmd[1]}") 
+    }
+end
+
+def edgeu_network(cmd)
+    lock.synchronize{
+        $rt.each {|node, str| 
+            if str[:dst] == cmd[0]
+                str[:cost] = cmd[1]
+            end
+        }
+    }
 end
 
 def status()
-    STDOUT.puts "STATUS: not implemented"
+=begin
+    string = "#{$hostname},#{port}"
+    lock.synchronize{
+        $rt.each {|node, str| string << ",#{str[:dst]}"} #Need to make sure this is lexical order
+    }
+=end
+    STDOUT.puts "STATUS: not implemented"#string
 end
 
 
@@ -94,6 +141,7 @@ end
 def main()
 
     while(line = STDIN.gets())
+        puts "Main() #{Thread.current}"
         line = line.strip()
         arr = line.split(' ')
         cmd = arr[0]
@@ -101,7 +149,7 @@ def main()
         case cmd
         when "EDGEB"; edgeb_stdin(args)
         when "EDGED"; edged(args)
-        when "EDGEW"; edgew(args)
+        when "EDGEU"; edgeu(args)
         when "DUMPTABLE"; dumptable(args)
         when "SHUTDOWN"; shutdown(args)
         when "STATUS"; status()
@@ -116,13 +164,50 @@ def main()
 
 end
 
+
+#A thread that handles all incoming connections
+def serverHandling()
+     
+    loop {
+        puts "ServerHandling thread: #{Thread.current}"
+        thread = Thread.start($serv.accept) do |client|
+           
+            message = client.gets.chomp
+
+            puts "THIS IS THE MESSAGE: #{message}"
+
+            arr = message.split(',')
+            server_cmd = arr[0]
+            args = arr[1..-1]
+
+            case server_cmd
+            when "EDGEB2"; edgeb_network(args)
+            when "EDGEU2"; edgeu_network(args)
+            when "EDGED2"; edged_network(args)
+            else STDERR.puts "ERROR: INVALID COMMAND \"#{server_cmd}\""
+            end
+
+        end
+
+        thread.join 
+    }
+
+
+end
+
+
+
+
+
 def setup(hostname, port, nodes, config)
     #set up ports, server, buffers
+    puts "Hostname: #{hostname}  Port: #{port.to_i}"
     $hostname = hostname
     $p = port.to_i
     $node_info = Struct.new(:src, :dst, :cost, :nexthop)
+    $record_ip = Hash.new
 
-    $serv = TCPServer.open($p) 
+    $serv = TCPServer.open($p)
     
     fHandle = File.open(nodes)
     while(line = fHandle.gets())
@@ -136,9 +221,23 @@ def setup(hostname, port, nodes, config)
     end
 
     $clock = Time.now
-    main()
 
+    puts "Main Thread: #{Thread.current}"
+
+    begin
+        t1 = Thread.new do
+            puts "Child thread: #{Thread.current}"
+            serverHandling()
+        end
+    end    
+  
+   main()
 end
+
+
+
+
+
 
 setup(ARGV[0], ARGV[1], ARGV[2], ARGV[3])
 
